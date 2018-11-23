a1f4e8c4f2ccf84b86dac6805e6d70bdb75d5149
viktorkanev <viktor.kanev9@gmail.com>
2018-11-23 22:04:35 +0000

    Edited custom.css, added custom logo

--- app/routes.py
+++ app/routes.py
@@ -80,7 +80,7 @@ def save_post_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
-    picture_path = os.path.join(app.root_path, 'static/avatars', picture_fn)
+    picture_path = os.path.join(app.root_path, 'static/post_pictures', picture_fn)
 
     i = Image.open(form_picture)
     i.save(picture_path)
